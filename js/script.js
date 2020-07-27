1170edd6af6fb55bfac2c6f68250c5647fdc9f8a
charlesmaillu <charles972@live.com>
2020-07-27 06:33:07 +0300

    add styles to portfolio

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      if ($(email).val() && $(message).val()){
         alert (name + ", we have received your message. Thank you for reaching out to us.");
       }
       else {
